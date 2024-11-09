a25a607b215bf36c950460dea17020e6f6049cc8
JangWooJin1 <jangwoojin1@gmail.com>
2024-11-09 11:08:11 +0900

    feat: navigation link to practice detail page (`/practice/1`) (#33)

--- src/pages/Practice.jsx
+++ src/pages/Practice.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import Main from './practice/Main'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import Main from './practice/Main';
 
 export default function Practice() {
-    return (
-      <Main />
-    )
+  const { id } = useParams(); 
+
+  return (
+    <Main practiceId={id} />
+  );
 }
--- src/pages/practice/Main.jsx
+++ src/pages/practice/Main.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import Logo from '../../components/Logo';
 import Assignment from './Assignment';
 import Answer from './Answer';
 import Submit from './Submit';
 
-export default function Main() {
+export default function Main({ practiceId }) {
   return (
     <div className="min-vh-100 bg-light">
       <header className="border-bottom">
-          <Logo />
+        <Logo />
       </header>
       <main>
         <Container className="py-4">
           <Row className="gy-4">
             <Col xs={12}>
-              <Assignment />
+              <Assignment practiceId={practiceId} />
             </Col>
             <Col xs={12}>
-              <Answer />
+              <Answer practiceId={practiceId} />
             </Col>
             <Col xs={12}>
-              <Submit />
+              <Submit practiceId={practiceId} />
             </Col>
           </Row>
         </Container>
